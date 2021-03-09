7038c69c98711cf7947a61502bc17587ca3f39f1
mattdean <matthewallandean@gmail.com>
2021-03-09 09:41:16 -0700

    Finsih post functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,19 +4,18 @@ export const getUrls = () => {
 }
 
 export const postUrl = (longUrl, title) => {
-const requestOptions = {
-  method: 'POST',
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    long_url: longUrl, 
-    title: title
-  })
-};
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      long_url: longUrl, 
+      title: title
+    })
+  };
 
-return fetch("http://localhost:3001/api/v1/urls", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  return fetch("http://localhost:3001/api/v1/urls", requestOptions)
+    .then(response => response.json())
+    .catch(error => console.log('error', error));
 }
